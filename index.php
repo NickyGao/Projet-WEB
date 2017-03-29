263f40dc5f6934c1d372805ac83a41218abe23f9
Krayt78 <ludovic.domingues96@gmail.com>
2017-03-29 16:34:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Page D'accueil</title>
+        <meta charset="utf-8" />
+        <link href="./style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header>
+            <h1>page d'accueil</h1>
+            <p>Réalisé Par :Domingues Ludovic</p>
+        </header>
+        <section >
+            <h3>veuillez choisir le TD que vous voulez y accéder :</h3>
+            <ul>
+                 <li><a href="./td05/td05.php">TD05</a></li>
+                 <li><a href="./TD_6/td6.php">TD06</a></li>
+                 <li><a href="./pages/page.html">TD07</a></li>
+                 <li><a href="./php/TD08.php">TD08</a></li>
+                 <li><a href="./TD09.html">TD09</a></li>
+                 <li><a href="./pages/page.html">TD10</a></li>
+                 <li><a href="./pages/page.html">TD11</a></li>
+                 <li><a href="./pages/page.html">TD12</a></li>
+            </ul>       
+        </section>
+        <section>
+            <h3>Autres TD:</h3>
+            <ul>
+             <li><a href="./rapport/index.html">TD01</a></li>
+             <li><a href="./NAAL_DOMINGUES/index.html">TD02</a></li>
+             <li><a href="./page-simple/index.html">TD03</a></li>
+            </ul>
+        
+        </section>
+        <section>
+            <h3>Autres Liens :</h3>
+            <ol>
+                <li>Accès au validateur Html5:<a href="https://validator.nu/">Cliquez Ici</a></li>
+                <li>Accès au validateur Css:<a href="https://jigsaw.w3.org/css-validator/">Cliquez Ici</a></li>
+                <li>Accès a la page phpinfo : <a href="td05/phpinfo.php">Cliquez Ici</a></li>
+            </ol>
+            
+        
+        </section>
+    
+    
+    
+    </body>
+    
+    
+    <footer>
+<div class="droit"><p>copyright &copy; 2017 - Tous droits réservés à leur propriétaires respectifs</p></div>
+
+        
+        <?php
+                include("./util.inc.php");
+        ?>
+        
+        
+  
+
+</footer>
+
+
+
+
+
+
+
+
+
+</html>
